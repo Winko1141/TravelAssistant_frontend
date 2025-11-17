6ed7b0c8cb0d80ebd87b69bee27f0641ee9fa46c
winko <2125981795@qq.com>
2025-11-17 15:38:43 +0800

    : 引入vant库

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.ts'
+import router from './router/index'
+import { Button } from 'vant'
+
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(Button)
 app.mount('#app')
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <p>聊天界面</p>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>  
   </div>
 </template>
 
